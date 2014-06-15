153457adcb436c4b27540fa5f4cec56e9ee651e5
Dheera Venkatraman <dheera@dheera.net>
2014-06-15 03:46:30 +0000

    cached decorator update

--- dheeranet/cache.py
+++ dheeranet/cache.py
@@ -1,17 +1,29 @@
 from flask import Flask,request
 from werkzeug.contrib.cache import FileSystemCache
+import marshal
 
-CACHE_TIMEOUT = 300
+CACHE_TIMEOUT = 3600
 cache = FileSystemCache('dheeranet/cache')
+
+def get_id(f, args, kwargs, mark=object()):
+    l = [id(f)]
+    for arg in args:
+        l.append(id(arg))
+    l.append(id(mark))
+    for k, v in kwargs:
+        l.append(k)
+        l.append(id(v))
+    return marshal.dumps(tuple(l))
 
 class cached(object):
   def __init__(self, timeout=None):
     self.timeout = timeout or CACHE_TIMEOUT
   def __call__(self, f):
     def decorator(*args, **kwargs):
-      response = cache.get(request.path)
+      key = get_id(f,args,kwargs)
+      response = cache.get(key)
       if response is None:
         response = f(*args, **kwargs)
-        cache.set(request.path, response, self.timeout)
+        cache.set(key, response, self.timeout)
       return response
     return decorator
--- dheeranet/views/photos.py
+++ dheeranet/views/photos.py
@@ -1,23 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask import Blueprint, render_template, abort, Response, send_file, send_from_directory
 from jinja2 import TemplateNotFound
 from subprocess import call
 from dheeranet import static_bucket
-from dheeranet.cache import cache
+from dheeranet.cache import cache, cached
 from dheeranet.slugify import slugify
 from boto.s3.key import Key
 import os, glob
 import json
 from hashlib import sha1
 
+PHOTOS_BUCKET = static_bucket
+PHOTOS_BUCKET_NAME = 'static.dheera.net'
+PHOTOS_PREFIX = 'photos/'
 PHOTOS_THUMB_WIDTH = '140'
 PHOTOS_THUMB_HEIGHT = '140'
+
 PHOTOS_THUMB_SIZE = PHOTOS_THUMB_WIDTH + 'x' + PHOTOS_THUMB_HEIGHT
 
 photos = Blueprint('photos', __name__,template_folder='../template')
 
-@photos.route('/', defaults={'album': ''})
+@photos.route('/')
+def show():
+  content = ''
+
+  featured_json = cache.get('photos:featured')
+  if not featured_json:
+    featured_key = PHOTOS_BUCKET.get_key('photos/featured')
+    if featured_key:
+      featured_json = featured_key.get_contents_as_string().decode('utf-8')
+      cache.set('photos:featured', featured_json)
+
+  featured = json.loads(featured_json)
+
+  for featured_section in featured:
+    content += '<h2>%s</h2>' % featured_section['title']
+
+  return render_template('page.html',title='{|en:photos|zh:相冊|}',content=content)
+
 @photos.route('/<path:album>')
-def show(album):
+def show_album(album):
   content = ''
   album = album.strip('/')
 
@@ -41,38 +65,26 @@ def show(album):
 
   return render_template('page.html',title=album_info['title'],content=content)
 
+@cached()
 def album_get_info(album):
-  info_json = cache.get('photos:info:' + album)
-  if not info_json:
-    info_key = static_bucket.get_key('photos/'+album+'/info')
-    if info_key:
-      info_json = info_key.get_contents_as_string().decode('utf-8')
-      cache.set('photos:info:' + album, info_json)
-    else:
-      return None
-
-  info = json.loads(info_json)
-  return info
-
-def album_get_filenames(album):
-  filenames = ()
-  filenames_json = cache.get('photos:filenames:' + album)
-
-  if filenames_json:
-    filenames = json.loads(filenames_json)
+  info_key = PHOTOS_BUCKET.get_key('photos/'+album+'/info')
+  if info_key:
+    return json.loads(info_key.get_contents_as_string().decode('utf-8'))
   else:
-    filenames = static_bucket.list('photos/'+album+'/web-1024/','/')
-    filenames = map(lambda(k): k.name.encode('utf-8'), filenames)
-    filenames = map(lambda(s): s[s.rfind('/')+1:], filenames)
-    cache.set('photos:filenames:' + album, json.dumps(filenames))
+    return None
 
+@cached()
+def album_get_filenames(album):
+  filenames = PHOTOS_BUCKET.list('photos/'+album+'/web-1024/','/')
+  filenames = map(lambda(k): k.name.encode('utf-8'), filenames)
+  filenames = map(lambda(s): s[s.rfind('/')+1:], filenames)
   return filenames
 
 def album_get_display_url(album,filename):
-  return "http://static.dheera.net/photos/%s/web-1024/%s" % (album, filename)
+  return "http://%s/%s%s/web-1024/%s" % (PHOTOS_BUCKET_NAME, PHOTOS_PREFIX, album, filename)
 
 def album_get_download_url(album,filename):
-  return "http://static.dheera.net/photos/%s/original/%s" % (album, filename)
+  return "http://%s/%s%s/original/%s" % (PHOTOS_BUCKET_NAME, PHOTOS_PREFIX, album, filename)
 
 def album_get_thumb_url(album,filename):
-  return "http://static.dheera.net/photos/%s/thumb-%s-%s/%s" % (album, PHOTOS_THUMB_WIDTH, PHOTOS_THUMB_HEIGHT, filename)
+  return "http://%s/%s%s/thumb-%s-%s/%s" % (PHOTOS_BUCKET_NAME, PHOTOS_PREFIX, album, PHOTOS_THUMB_WIDTH, PHOTOS_THUMB_HEIGHT, filename)
