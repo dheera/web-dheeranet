091189b636d3514900464e78b9d9e1a08fb4d619
Dheera Venkatraman <dheera@dheera.net>
2014-07-08 16:44:21 +0000

    home news

--- dheeranet/views/home.py
+++ dheeranet/views/home.py
@@ -1,10 +1,14 @@
 from flask import Blueprint, render_template, abort, request
 from jinja2 import TemplateNotFound
-
-import json
+from dheeranet import static_bucket
+from dheeranet.cache import s3_get_cached
+import json, datetime
 
 home = Blueprint('home', __name__,template_folder='../template')
 
 @home.route('/')
 def show():
-  return render_template('home.html')
+  home_items = json.loads(s3_get_cached(static_bucket, '__home__'))
+  news_items = filter(lambda x:x['type']=='news', home_items)
+
+  return render_template('home.html', news_items = news_items)
